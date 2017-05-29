a6a896cefb5b584ab21e85263baa22043373efc3
mikerosner <mikerosner@users.noreply.github.com>
2017-05-28 23:24:13 -0400

    Update pythonAMI.bash

--- pythonAMI.bash
+++ pythonAMI.bash
@@ -3,10 +3,10 @@ sudo yum install numpy
 sudo yum install scipy
 sudo yum install gcc
 
+wget "https://repo.continuum.io/archive/Anaconda2-4.3.1-Linux-x86_64.sh"
+bash Anaconda2-4.3.1-Linux-x86_64.sh
 
 wget "http://effbot.org/downloads/Imaging-1.1.7.tar.gz"
 tar -xzf Imaging-1.1.7.tar.gz
 cd Imaging-1.1.7
-./configure
-make
-sudo make install
+sudo python26 setup.py install
